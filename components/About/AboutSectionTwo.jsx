54a9ae35588cee0327466ac33a1c7c81d48b06ca
hs_team <svekariy@rhrk.uni-kl.de>
2023-02-17 16:37:26 +0100

    first commit

---
+++ components/About/AboutSectionTwo.jsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+
+const AboutSectionTwo = () => {
+  return (
+    <section className="py-16 md:py-20 lg:py-28">
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap items-center">
+          <div className="w-full px-4 lg:w-1/2">
+            <div
+              className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
+              data-wow-delay=".15s"
+            >
+              <Image
+                src="/images/about/about-image-2.svg"
+                alt="about image"
+                fill
+              />
+            </div>
+          </div>
+          <div className="w-full px-4 lg:w-1/2">
+            <div className="wow fadeInUp max-w-[470px]" data-wow-delay=".2s">
+              <div className="mb-9">
+                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  Bug free code
+                </h3>
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
+                It's not always easy to achieve, but writing bug-free code is a great feeling. When you succeed, it's a real accomplishment that can make you proud.
+                </p>
+              </div>
+              <div className="mb-9">
+                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  Premier support
+                </h3>
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
+                FebGeeks Premier Support Advanced will provide best practices, advice and guidance regarding the products under the Premier Support agreement.
+                </p>
+              </div>
+              <div className="mb-1">
+                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                Performance Optimization
+                </h3>
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
+                Customers don’t appreciate slow and clumsy online stores. Especially millennials and gen-z are looking for lighting fast online stores. The majority of search engines take page speed into account when ranking websites. Hence, boosting the performance of your online store is now more crucial than ever.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSectionTwo;
